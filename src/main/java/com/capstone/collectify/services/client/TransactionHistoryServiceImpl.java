b928fd527abe75ce6a318ac620f660902402a316
kbdiamondes <kbdiamantes@gmail.com>
2023-10-23 16:50:41 +0800

    coldnoon_dev1023v448pm
    # Added
    
    ### Feature
    
    - Transaction History Feature for Clients
    
    ### Additions
    - TransactionHistoryService & ServiceImpl
    - TransactionHistoryController

---
+++ src/main/java/com/capstone/collectify/services/client/TransactionHistoryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.capstone.collectify.services.client;
+
+import com.capstone.collectify.models.TransactionHistory;
+import com.capstone.collectify.repositories.TransactionHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionHistoryServiceImpl implements TransactionHistoryService {
+    @Autowired
+    private TransactionHistoryRepository transactionHistoryRepository;
+
+    @Override
+    public List<TransactionHistory> getAllTransactionsByClient(Long clientId) {
+        return transactionHistoryRepository.findByClientId(clientId);
+    }
+}
