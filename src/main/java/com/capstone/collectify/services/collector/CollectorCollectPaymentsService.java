74d8148a1dda8403e4c55e708ab384a275eddada
kbdiamondes <kbdiamantes@gmail.com>
2023-10-24 11:40:56 +0800

    coldmorning_dev1024v11:40am
    # Added
    
    ## Feature
    
    ### Collect All Payments [Collector]
    
    - Added collect all contracts for collector
    
    ## Issues
    
    - "client_client_id" & "reseller_reseller_id" is not recorded on collection history when collector collects

--- src/main/java/com/capstone/collectify/services/collector/CollectorCollectPaymentsService.java
+++ src/main/java/com/capstone/collectify/services/collector/CollectorCollectPaymentsService.java
@@ -5,4 +5,5 @@ import java.nio.file.AccessDeniedException;
 
 public interface CollectorCollectPaymentsService {
     void collectPayments(Long collectorId, Long contractId, String paymentType, String base64ImageData, String fileName, String contentType ) throws AccessDeniedException, IOException;
+    void collectPaymentsFromAllContracts(Long collectorId, String paymentType, String base64ImageData, String fileName, String contentType) throws AccessDeniedException, IOException;
 }
