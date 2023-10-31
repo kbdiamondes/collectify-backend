69a0a591f3ff09c75d6310d86e34ce230d5359e8
Keith <kbdiamantes@gmail.com>
2023-10-31 21:14:18 +0800

    coldnight_dev1031913PM
    # Added
    
    - PaymentRecords -> returns collector fullname

--- src/main/java/com/capstone/collectify/services/client/PaymentRecordsServiceImpl.java
+++ src/main/java/com/capstone/collectify/services/client/PaymentRecordsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.capstone.collectify.services.client;
 
 import com.capstone.collectify.models.Client;
+import com.capstone.collectify.models.Collector;
+import com.capstone.collectify.models.Contract;
 import com.capstone.collectify.models.TransactionHistory;
 import com.capstone.collectify.repositories.TransactionHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +19,25 @@ public class PaymentRecordsServiceImpl implements PaymentRecordsService {
 
     @Override
     public List<TransactionHistory> getPaymentRecordsForClient(Client client) {
-        return transactionHistoryRepository.findPaymentRecordsWithZeroAmount(client.getClient_id());
+        List<TransactionHistory> paymentRecords = transactionHistoryRepository.findPaymentRecordsWithZeroAmount(client.getClient_id());
+
+        for (TransactionHistory record : paymentRecords) {
+            // Fetch the associated contract for each payment record
+            Contract contract = record.getContract();
+
+            if (contract != null) {
+                Collector collector = contract.getCollector();
+                if (collector != null) {
+                    // If there's a Collector, set the collector's full name
+                    record.setCollectorName(collector.getFullName());
+                } else {
+                    // If no Collector is assigned, set the Reseller's username
+                    record.setCollectorName(contract.getReseller().getFullName());
+                }
+            }
+        }
+
+        return paymentRecords;
     }
 
 }
