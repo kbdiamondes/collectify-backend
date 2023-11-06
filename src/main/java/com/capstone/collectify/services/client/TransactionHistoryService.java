7f0d932052fc155aabfb0fa0a1e133caf1bbf53e
Keith <kbdiamantes@gmail.com>
2023-11-06 22:27:53 +0800

    Get ALL Transaction HISTORY

--- src/main/java/com/capstone/collectify/services/client/TransactionHistoryService.java
+++ src/main/java/com/capstone/collectify/services/client/TransactionHistoryService.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface TransactionHistoryService {
 
     List<TransactionHistory> getAllTransactionsByClient(Long clientId);
+
+    List<TransactionHistory> getAllTransactions();
+
 }
